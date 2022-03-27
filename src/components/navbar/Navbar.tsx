ac43eb51266183d0142be349c333fbdd3ef5eab3
Jeffrey Polasz <jeff_polasz@hotmail.com>
2022-03-27 01:51:05 -0400

    Render basic text elements on navbar menu toggle

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,12 @@
-import { Box, Flex, useColorModeValue, useDisclosure } from "@chakra-ui/react";
+import {
+  Box,
+  Collapse,
+  Flex,
+  Stack,
+  Text,
+  useColorModeValue,
+  useDisclosure,
+} from "@chakra-ui/react";
 import Logo from "./Logo";
 import MenuToggle from "./MenuToggle";
 
@@ -19,9 +27,79 @@ const Navbar = () => {
       >
         <Logo logoText="Jeffrey Polasz" />
         <MenuToggle onToggle={onToggle} isOpen={isOpen} />
+
+        <Collapse in={isOpen} animateOpacity>
+          <MobileNavDropdown />
+        </Collapse>
       </Flex>
     </Box>
   );
 };
+
+const MobileNavDropdown = () => {
+  return (
+    <Stack
+      bg={useColorModeValue("white", "gray.800")}
+      p={4}
+      display={{ md: "none" }}
+    >
+      {NAV_ITEMS.map((navItem) => (
+        <MobileNavItem key={navItem.label} {...navItem} />
+      ))}
+    </Stack>
+  );
+};
+
+const MobileNavItem: React.FC<NavItemProps> = ({ label }) => {
+  return <Text>{label}</Text>;
+};
+
+interface NavItemProps {
+  label: string;
+  subLabel?: string;
+  children?: Array<NavItemProps>;
+  href?: string;
+}
+
+const NAV_ITEMS: Array<NavItemProps> = [
+  {
+    label: "Inspiration",
+    children: [
+      {
+        label: "Explore Design Work",
+        subLabel: "Trending Design to inspire you",
+        href: "#",
+      },
+      {
+        label: "New & Noteworthy",
+        subLabel: "Up-and-coming Designers",
+        href: "#",
+      },
+    ],
+  },
+  {
+    label: "Find Work",
+    children: [
+      {
+        label: "Job Board",
+        subLabel: "Find your dream design job",
+        href: "#",
+      },
+      {
+        label: "Freelance Projects",
+        subLabel: "An exclusive list for contract work",
+        href: "#",
+      },
+    ],
+  },
+  {
+    label: "Learn Design",
+    href: "#",
+  },
+  {
+    label: "Hire Designers",
+    href: "#",
+  },
+];
 
 export default Navbar;
