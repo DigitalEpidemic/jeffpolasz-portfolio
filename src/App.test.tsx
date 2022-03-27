69c5562818319643bbdb0578d1b73db3d7a012b4
Jeffrey Polasz <jeff_polasz@hotmail.com>
2022-03-26 22:45:24 -0400

    Remove boilerplate code and setup basic test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Hello, World! text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+
+  const linkElement = screen.getByText(/hello, world!/i);
   expect(linkElement).toBeInTheDocument();
 });
