61f540fad166d14bc28fe0587d1fd68649811987
Jeffrey Polasz <jeff_polasz@hotmail.com>
2022-06-06 17:26:20 -0400

    Add test to not render excluded filters

--- src/components/portfolio/Portfolio.test.tsx
+++ src/components/portfolio/Portfolio.test.tsx
@@ -4,28 +4,43 @@ import { CardProps } from "./Card";
 import Portfolio from "./Portfolio";
 
 describe("Portfolio", () => {
+  const mockPortfolioData: CardProps[] = [
+    {
+      filter: ["Unity", "Android", "iOS", "Other"],
+      title: "Shinobi Jump",
+      thumbnail: "https://jeffpolasz.com/images/shinobi-jump-featuredimg.jpg",
+      feature1: "Hand drawn UI and game art",
+      feature2: "Perfected using a dark colour palette",
+      feature3: "Developed & published in 10 days",
+      googlePlay:
+        "https://play.google.com/store/apps/details?id=com.adknown.shinobijump",
+      appStore: "https://apps.apple.com/ca/app/shinobi-jump/id1502583004",
+    },
+  ];
+  const title = "Game Stuff";
+
   it("renders Portfolio and mock data", () => {
-    const mockPortfolioData: CardProps[] = [
-      {
-        filter: ["unity", "android", "ios"],
-        title: "Shinobi Jump",
-        thumbnail: "https://jeffpolasz.com/images/shinobi-jump-featuredimg.jpg",
-        feature1: "Hand drawn UI and game art",
-        feature2: "Perfected using a dark colour palette",
-        feature3: "Developed & published in 10 days",
-        googlePlay:
-          "https://play.google.com/store/apps/details?id=com.adknown.shinobijump",
-        appStore: "https://apps.apple.com/ca/app/shinobi-jump/id1502583004",
-      },
-    ];
-
-    const title = "Game Stuff";
-
     render(<Portfolio title={title} portfolioData={mockPortfolioData} />);
 
     const portfolioTitle = screen.getByText(title);
     expect(portfolioTitle).toBeInTheDocument();
 
     expect(screen.getByText(mockPortfolioData[0].title)).toBeInTheDocument();
+  });
+
+  it("does not render excluded filters", () => {
+    const excludeFilters = ["Other"];
+
+    render(
+      <Portfolio
+        title={title}
+        portfolioData={mockPortfolioData}
+        excludeFilters={excludeFilters}
+      />
+    );
+
+    const otherFilter = screen.queryByText(excludeFilters[0]);
+
+    expect(otherFilter).not.toBeInTheDocument();
   });
 });
