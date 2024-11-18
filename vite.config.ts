0e88275f6494e2b290180c100fb23d24b33e0bfc
Jeffrey Polasz <jeff_polasz@hotmail.com>
2024-11-18 00:20:37 -0500

    Migrate to vitest

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    environment: "jsdom",
+    globals: true, // Allows using global variables like `describe` and `it`
+    setupFiles: "src/setupTests.ts",
+  },
   plugins: [react()],
 });
