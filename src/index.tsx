d30e3aaa2a4c0bbbe267d6bd30bfc797f3476bc4
Jeffrey Polasz <jeff_polasz@hotmail.com>
2024-11-29 17:08:55 -0500

    Implement react-router with basic route to home page

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router";
 import App from "./App";
 import { NavbarProvider } from "./providers/NavbarProvider";
 
 const container = document.getElementById("root");
-const root = createRoot(container as HTMLElement);
+const root = ReactDOM.createRoot(container as HTMLElement);
 
 root.render(
   <React.StrictMode>
     <ChakraProvider>
       <NavbarProvider>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </NavbarProvider>
     </ChakraProvider>
   </React.StrictMode>
---
+++ src/pages/Home.test.tsx
@@ -0,0 +1,28 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { render, screen } from "@testing-library/react";
+import { Home } from "./Home";
+
+describe("Home", () => {
+  // TODO: Change for something real on the page
+  it("renders Hello, World! text", () => {
+    render(
+      <ChakraProvider>
+        <Home />
+      </ChakraProvider>
+    );
+
+    const linkElement = screen.getByText(/hello, world!/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it("hides horizontal scrollbar", () => {
+    render(
+      <ChakraProvider>
+        <Home />
+      </ChakraProvider>
+    );
+
+    const homeContainer = screen.getByTestId("Home");
+    expect(getComputedStyle(homeContainer).overflowX).toBe("hidden");
+  });
+});
