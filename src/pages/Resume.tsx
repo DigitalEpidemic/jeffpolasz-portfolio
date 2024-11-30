4c0b7c9365f14c0b665e17a6461447024e7457a2
Jeffrey Polasz <jeff_polasz@hotmail.com>
2024-11-29 19:40:49 -0500

    Add resume preview to page

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,3 +1,20 @@
+import { Box, Stack } from "@chakra-ui/react";
+import Footer from "../components/footer/Footer";
+import Navbar from "../components/navbar/Navbar";
+
 export const Resume = () => {
-  return <div>Download Resume as .PDF</div>;
+  return (
+    <Box h={"100vh"} data-testid="Resume">
+      <Navbar sticky />
+      <Stack mt={"65px"} h={"100%"}>
+        <div>Download Resume as .PDF</div>
+        <iframe
+          width={"100%"}
+          height={"100%"}
+          src="https://drive.google.com/file/d/16KzzDWwR7_TX2BmeobE4-uihwT1OnYiD/preview?pli=1"
+        />
+      </Stack>
+      <Footer />
+    </Box>
+  );
 };
